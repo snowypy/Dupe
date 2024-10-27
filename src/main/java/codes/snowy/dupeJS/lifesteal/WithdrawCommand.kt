6f091f914076aca8790e90465807e29944b6ba06
snowypy <me@snowy.codes>
2024-10-27 11:26:49 +0000

    Impl NBTItem @ WithdrawCommand.kt

--- src/main/java/codes/snowy/dupeJS/lifesteal/WithdrawCommand.kt
+++ src/main/java/codes/snowy/dupeJS/lifesteal/WithdrawCommand.kt
@@ -4,13 +4,19 @@ import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.CommandAlias
 import co.aikar.commands.annotation.Default
 import co.aikar.commands.annotation.Optional
+import codes.snowy.dupeJS.DupeJS
+import codes.snowy.dupeJS.utils.Config
 import codes.snowy.dupeJS.utils.translate
+import de.tr7zw.changeme.nbtapi.NBTItem
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 @CommandAlias("withdraw")
 class WithdrawCommand(private val manager: LifestealManager) : BaseCommand() {
+
+
+    private val config = Config(DupeJS.getInstance())
 
     @Default
     fun onWithdraw(player: Player, @Optional amount: Int?) {
@@ -31,9 +37,12 @@ class WithdrawCommand(private val manager: LifestealManager) : BaseCommand() {
 
         val heartItem = ItemStack(Material.RED_DYE, heartsToWithdraw).apply {
             itemMeta = itemMeta?.apply { setDisplayName("§cHeart") }
-        }
 
-        player.inventory.addItem(heartItem)
+        }
+        val nbtItem = NBTItem(heartItem)
+        nbtItem.setInteger("custom_model_data", config.getInt("dupe.modeldata", 1111))
+
+        player.inventory.addItem(nbtItem.item)
         player.sendMessage("&#f6294b&lLIFESTEAL &8| &aYou have withdrawn $heartsToWithdraw heart(s).".translate())
     }
 }
