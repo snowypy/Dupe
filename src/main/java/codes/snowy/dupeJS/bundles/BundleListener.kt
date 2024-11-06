859e308050908a4a248669bb09aec9aa119c21b4
snowy <git@snowyjs.lol>
2024-11-06 23:16:00 +0000

    Added strip color + hex

--- src/main/java/codes/snowy/dupeJS/bundles/BundleListener.kt
+++ src/main/java/codes/snowy/dupeJS/bundles/BundleListener.kt
@@ -1,6 +1,8 @@
 package codes.snowy.dupeJS.bundles
 
 import codes.snowy.dupeJS.bundles.BundleManager
+import org.bukkit.ChatColor
+import org.bukkit.Color
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEvent
@@ -12,11 +14,13 @@ class BundleListener : Listener {
         val player = event.player
         val item = event.item ?: return
         val bundleName = item.itemMeta?.displayName ?: return
+        val bundleNames = ChatColor.stripColor(bundleName).toString()
+        val bundleStripped = bundleNames.replace(" Bundle", "")
 
-        val bundle = BundleManager.getBundle(bundleName)
+        val bundle = BundleManager.getBundle(bundleStripped)
         if (bundle != null) {
             event.isCancelled = true
-            BundleGUI.openBundleGUI(player, bundle)
+            BundleGUI.openBundleAnimation(player, bundle)
         }
     }
 }
