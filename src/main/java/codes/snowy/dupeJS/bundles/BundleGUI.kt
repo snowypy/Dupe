5a0432aa8c20ff25c0344f25c8721aa3809a487e
snowy <git@snowyjs.lol>
2024-11-07 23:04:39 +0000

    Impl list @ BundleGUI

--- src/main/java/codes/snowy/dupeJS/bundles/BundleGUI.kt
+++ src/main/java/codes/snowy/dupeJS/bundles/BundleGUI.kt
@@ -6,8 +6,11 @@ import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scheduler.BukkitTask
 
 object BundleGUI {
+
+    private val ongoingAnimations = mutableMapOf<Player, BukkitTask>()
 
     fun openBundlePreview(player: Player, bundle: Bundle) {
         val inventory = Bukkit.createInventory(BundleInventoryHolder(bundle, "preview"), 27, "${bundle.color}Preview: ${bundle.displayName} ${bundle.color}Bundle!".translate())
@@ -23,20 +26,20 @@ object BundleGUI {
         val inventory = Bukkit.createInventory(BundleInventoryHolder(bundle, "opening"), 27, "${bundle.color}Opening: ${bundle.displayName} ${bundle.color}Bundle!".translate())
         player.openInventory(inventory)
 
-        object : BukkitRunnable() {
+        val animationTask = object : BukkitRunnable() {
             var ticks = 0
             var selectedItem: ItemStack? = null
 
             override fun run() {
                 if (ticks >= 10) {
-                    inventory.clear()
-                    selectedItem = bundle.items.random()
-                    inventory.setItem(13, selectedItem)
+                    if (removeBundleItem(player, bundle)) {
+                        inventory.clear()
+                        selectedItem = bundle.items.random()
+                        inventory.setItem(13, selectedItem)
+                        selectedItem?.let { player.inventory.addItem(it) }
+                    }
 
-                    selectedItem?.let { player.inventory.addItem(it) }
-
-                    removeBundleItemFromPlayer(player, bundle)
-
+                    ongoingAnimations.remove(player)
                     cancel()
                 } else {
                     for (i in 0 until inventory.size) {
@@ -46,19 +49,26 @@ object BundleGUI {
                 }
             }
         }.runTaskTimer(DupeJS.getInstance(), 0L, 5L)
+
+        ongoingAnimations[player] = animationTask
     }
 
-    private fun removeBundleItemFromPlayer(player: Player, bundle: Bundle) {
+     fun removeBundleItem(player: Player, bundle: Bundle): Boolean {
         val inventory = player.inventory
 
         for (item in inventory.contents) {
             if (item != null && item.hasItemMeta()) {
                 val displayName = item.itemMeta?.displayName
-                if (displayName == "${bundle.color}${bundle.displayName}&r &fBundle".translate()) {
-                    inventory.remove(item)
-                    break
+                if (displayName == "${bundle.color}&l${bundle.displayName}&r &fBundle".translate()) {
+                    item.amount = item.amount - 1
+                    if (item.amount <= 0) {
+                        inventory.remove(item)
+                    }
+                    return true
                 }
             }
         }
+        return false
     }
+
 }
