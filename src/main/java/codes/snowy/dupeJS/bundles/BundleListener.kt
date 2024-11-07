6bad336a669e28fad331dc5001db05383ef99004
snowy <git@snowyjs.lol>
2024-11-07 21:45:21 +0000

    Added Actions and InventoryHandler to Listener.

--- src/main/java/codes/snowy/dupeJS/bundles/BundleListener.kt
+++ src/main/java/codes/snowy/dupeJS/bundles/BundleListener.kt
@@ -1,26 +1,64 @@
 package codes.snowy.dupeJS.bundles
 
+import codes.snowy.dupeJS.bundles.BundleGUI
+import codes.snowy.dupeJS.bundles.BundleInventoryHolder
 import codes.snowy.dupeJS.bundles.BundleManager
 import org.bukkit.ChatColor
-import org.bukkit.Color
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.entity.Player
 
 class BundleListener : Listener {
+
     @EventHandler
     fun onBundleOpen(event: PlayerInteractEvent) {
         val player = event.player
         val item = event.item ?: return
         val bundleName = item.itemMeta?.displayName ?: return
-        val bundleNames = ChatColor.stripColor(bundleName).toString()
-        val bundleStripped = bundleNames.replace(" Bundle", "")
+        val bundleStripped = ChatColor.stripColor(bundleName)?.replace(" Bundle", "") ?: return
 
         val bundle = BundleManager.getBundle(bundleStripped)
         if (bundle != null) {
+
             event.isCancelled = true
-            BundleGUI.openBundleAnimation(player, bundle)
+
+            when (event.action) {
+                Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> BundleGUI.openBundlePreview(player, bundle)
+                Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> BundleGUI.openBundleAnimation(player, bundle)
+                else -> return
+            }
+        }
+    }
+
+    @EventHandler
+    fun onInventoryClick(event: InventoryClickEvent) {
+        val inventory = event.clickedInventory ?: return
+
+        if (inventory.holder is BundleInventoryHolder) {
+            val bundleHolder = inventory.holder as BundleInventoryHolder
+
+            event.isCancelled = true
+
+            when (bundleHolder.type) {
+                "preview" -> {
+                    // [$] Add logging at some point
+                }
+                "opening" -> {
+                    // [$] Add logging at some point
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    fun onInventoryClose(event: InventoryCloseEvent) {
+        val inventory = event.inventory
+
+        if (inventory.holder is BundleInventoryHolder) {
+            val bundleHolder = inventory.holder as BundleInventoryHolder
         }
     }
 }
