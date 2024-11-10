b8b7cd9705abc30f3961b92bc7231f50dd5bfde5
snowy <git@snowyjs.lol>
2024-11-10 09:09:05 +0000

    Impl Syntax

--- src/main/java/codes/snowy/dupeJS/dupe/DupeBlacklistCommand.kt
+++ src/main/java/codes/snowy/dupeJS/dupe/DupeBlacklistCommand.kt
@@ -1,16 +1,18 @@
 package codes.snowy.dupeJS.dupe
 
 import co.aikar.commands.BaseCommand
-import co.aikar.commands.annotation.CommandAlias
-import co.aikar.commands.annotation.CommandCompletion
-import co.aikar.commands.annotation.CommandPermission
-import co.aikar.commands.annotation.Default
-import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.*
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 @CommandAlias("blacklistdupe")
 class DupeBlacklistCommand(val dupeManager: DupeManager) : BaseCommand() {
+
+    @HelpCommand
+    @Syntax("[query]")
+    fun help(sender: CommandSender, help: co.aikar.commands.CommandHelp) {
+        help.showHelp()
+    }
 
     @Default
     @CommandPermission("staff.blacklist")
