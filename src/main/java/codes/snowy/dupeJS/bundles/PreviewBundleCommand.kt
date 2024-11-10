6ff6194596163e6f44f17188f8bc3b750626e70d
snowy <git@snowyjs.lol>
2024-11-10 09:09:14 +0000

    Impl Syntax

--- src/main/java/codes/snowy/dupeJS/bundles/PreviewBundleCommand.kt
+++ src/main/java/codes/snowy/dupeJS/bundles/PreviewBundleCommand.kt
@@ -2,11 +2,18 @@ package codes.snowy.dupeJS.bundles
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.*
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 @CommandAlias("previewbundle")
 @CommandPermission("bundle.preview")
 class PreviewBundleCommand : BaseCommand() {
+
+    @HelpCommand
+    @Syntax("[query]")
+    fun help(sender: CommandSender, help: co.aikar.commands.CommandHelp) {
+        help.showHelp()
+    }
 
     @Default
     @CommandCompletion("@bundles")
