c2a76d33c138f76575156deff1c9c1ca1bc0068f
snowypy <git@snowyjs.lol>
2025-02-02 22:11:05 +0000

    v2.1

--- src/main/java/codes/snowy/dupeJS/utils/Logger.kt
+++ src/main/java/codes/snowy/dupeJS/utils/Logger.kt
@@ -4,15 +4,15 @@ import org.bukkit.Bukkit
 
 object Logger {
     fun log(msg: String, status: String) {
-        if ("$status" == "err") {
+        if (status == "err" || status == "error") {
             Bukkit.getConsoleSender().sendMessage("&#ae4ff7[DUPE] &#FF0000[ERROR] &c$msg".translate())
-        } else if ("$status" == "warning") {
+        } else if (status == "warning") {
             Bukkit.getConsoleSender().sendMessage("&#ae4ff7[DUPE] &#ed480c[WARNING] &#f78928$msg".translate())
-        } else if ("$status" == "success") {
+        } else if (status == "success") {
             Bukkit.getConsoleSender().sendMessage("&#ae4ff7[DUPE] &#0ced35[SUCCESS] &a$msg".translate())
-        } else if ("$status" == "info") {
+        } else if (status == "info") {
             Bukkit.getConsoleSender().sendMessage("&#ae4ff7[DUPE] &#03b1fc[INFO] &#69cefa$msg".translate())
-        } else if ("$status" == "debug"){
+        } else if (status == "debug"){
             Bukkit.getConsoleSender().sendMessage("&#ae4ff7[DUPE] &#03b1fc[DEBUG] &#69cefa$msg".translate())
         }
     }
---
+++ src/main/java/codes/snowy/dupeJS/utils/PlaceholderHandler.kt
@@ -0,0 +1,41 @@
+package codes.snowy.dupeJS.utils
+
+import codes.snowy.dupeJS.DupeJS
+import codes.snowy.dupeJS.dupe.DupeManager
+import me.clip.placeholderapi.expansion.PlaceholderExpansion
+import org.bukkit.entity.Player
+
+class PlaceholderHandler(plugin: DupeJS) : PlaceholderExpansion() {
+    private val plugin: DupeJS = plugin
+    private val dupeManager = DupeManager()
+
+    override fun getAuthor(): String {
+        return "snowi"
+    }
+
+    override fun getIdentifier(): String {
+        return "dupey"
+    }
+
+    override fun getVersion(): String {
+        return "1.0.0"
+    }
+
+    override fun persist(): Boolean {
+        return true
+    }
+
+    fun onRequest(player: Player, params: String): String? {
+        if (params.equals("dupe-charges", ignoreCase = true)) {
+            val charges = (dupeManager.getMaxDupeCount(player) - dupeManager.getMaxDupeCount(player)).toString()
+            return charges
+        } else if (params.equals("dupe-max-charges", ignoreCase = true)) {
+            val maxCharges = dupeManager.getMaxDupeCount(player).toString()
+            return maxCharges
+        } else if (params.equals("dupe-recharge-time", ignoreCase = true)) {
+            val rechargeTime = dupeManager.getRechargeTime(player).toString()
+            return rechargeTime
+        }
+        return "&cInvalid placeholder.".translate()
+    }
+}
