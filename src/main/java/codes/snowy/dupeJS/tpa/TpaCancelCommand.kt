4639783935cd5b55d93ec26b26946dbdd3b9c254
snowypy <git@snowyjs.lol>
2025-02-04 21:51:20 +0000

    v2.6.8 (Tpa Cancel Command)

---
+++ src/main/java/codes/snowy/dupeJS/tpa/TpaCancelCommand.kt
@@ -0,0 +1,38 @@
+package codes.snowy.dupeJS.tpa
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.Default
+import codes.snowy.dupeJS.utils.translate
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("tpacancel|tpcancel")
+class TpaCancelCommand: BaseCommand() {
+
+    private val tpaManager = TpaManager()
+
+    @Default
+    @CommandCompletion("@players")
+    fun onTpaCancelCommand(acceptee: CommandSender, senderName: String) {
+        if (acceptee !is Player) {
+            acceptee.sendMessage("&cOnly players can use this command.".translate())
+            return
+        }
+
+        if (senderName == null) {
+            acceptee.sendMessage("&cPlease specify a player to teleport to.".translate())
+            return
+        }
+
+        val sender = Bukkit.getPlayer(senderName)
+        if (sender == null) {
+            acceptee.sendMessage("&cPlayer not found.".translate())
+            return
+        }
+
+        tpaManager.cancelTeleport(acceptee, sender)
+    }
+}
