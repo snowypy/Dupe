9f873cd6a7732f419d5436973205b9e43be623f8
snowypy <git@snowyjs.lol>
2025-02-19 22:35:59 +0000

    v3.20.1 (Add CommandCompletions to warps)

--- src/main/java/codes/snowy/dupeJS/utils/CommandCompletions.kt
+++ src/main/java/codes/snowy/dupeJS/utils/CommandCompletions.kt
@@ -3,9 +3,11 @@ package codes.snowy.dupeJS.utils
 import co.aikar.commands.PaperCommandManager
 import codes.snowy.dupeJS.bundles.BundleManager
 import codes.snowy.dupeJS.kits.KitManager
+import codes.snowy.dupeJS.warp.WarpCommand
+import codes.snowy.dupeJS.warp.WarpManager
 import org.bukkit.ChatColor
 
-class CommandCompletions(private val kitManager: KitManager) {
+class CommandCompletions(private val kitManager: KitManager, private val warpManager: WarpManager) {
 
     fun register(manager: PaperCommandManager) {
         manager.commandCompletions.registerCompletion("colors") {
@@ -18,6 +20,11 @@ class CommandCompletions(private val kitManager: KitManager) {
 
         manager.commandCompletions.registerCompletion("kits") {
             kitManager.getAllKits().map { it.name }
+        }
+
+        manager.commandCompletions.registerCompletion("warps") {
+            val warpNames = warpManager.getAllWarpNames()
+            warpNames.map { it.lowercase() }
         }
     }
 }
