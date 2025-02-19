2135d7115c764b515cd26f8f254c91e130fcec88
snowypy <git@snowyjs.lol>
2025-02-19 22:37:33 +0000

    v3.20.8 (Create eco command)

---
+++ src/main/java/codes/snowy/dupeJS/economy/EconomyCommand.kt
@@ -0,0 +1,63 @@
+package codes.snowy.dupeJS.economy
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.*
+import codes.snowy.dupeJS.utils.translate
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("economy")
+class EconomyCommand : BaseCommand() {
+
+    @HelpCommand
+    fun help(sender: CommandSender, help: co.aikar.commands.CommandHelp) {
+        help.showHelp()
+    }
+
+    @Subcommand("add")
+    @Syntax("<player> <amount>")
+    @Description("Add money to a player's account")
+    fun addMoney(sender: CommandSender, playerName: String, amount: Double) {
+        val target = Bukkit.getPlayerExact(playerName)
+        if (target == null) {
+            sender.sendMessage("&#FF0000&lERROR &8| &fCouldn't find a player by the name &#ff0000&n$playerName".translate())
+            return
+        }
+
+        if (!VaultHook.hasEconomy()) {
+            sender.sendMessage("&#FF0000&lERROR &8| &fThere was an error with the Economy.".translate())
+            return
+        }
+
+        val result = VaultHook.deposit(target, amount)
+        if (result.isEmpty()) {
+            sender.sendMessage("&#00FF00&lSUCCESS &8| &fGave ${target.name} a value of &#00FF00&n$${amount.toInt()}".translate())
+        } else {
+            sender.sendMessage("&#FF0000&lERROR &8| &fCouldn't add the money: $result".translate())
+        }
+    }
+
+    @Subcommand("remove")
+    @Syntax("<player> <amount>")
+    @Description("Remove money from a player's account")
+    fun removeMoney(sender: CommandSender, playerName: String, amount: Double) {
+        val target = Bukkit.getPlayerExact(playerName)
+        if (target == null) {
+            sender.sendMessage("&#FF0000&lERROR &8| &fCouldn't find a player by the name &#ff0000&n$playerName".translate())
+            return
+        }
+
+        if (!VaultHook.hasEconomy()) {
+            sender.sendMessage("&#FF0000&lERROR &8| &fThere was an error with the Economy.".translate())
+            return
+        }
+
+        val result = VaultHook.withdraw(target, amount)
+        if (result.isEmpty()) {
+            sender.sendMessage("&#00FF00&lSUCCESS &8| &fTook &#00FF00&n$${amount.toInt()}&f from $playerName".translate())
+        } else {
+            sender.sendMessage("&#FF0000&lERROR &8| &fCouldn't take the money: $result".translate())
+        }
+    }
+} 
