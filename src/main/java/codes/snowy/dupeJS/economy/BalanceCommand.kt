8dcfe82d80c92eccf95eda48a2a5eeb6acd6c04a
snowypy <git@snowyjs.lol>
2025-02-20 12:05:09 +0000

    v3.20.11 (Add Balance Command)

---
+++ src/main/java/codes/snowy/dupeJS/economy/BalanceCommand.kt
@@ -0,0 +1,37 @@
+package codes.snowy.dupeJS.economy
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.*
+import codes.snowy.dupeJS.utils.translate
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+@CommandAlias("balance|bal")
+class BalanceCommand : BaseCommand() {
+
+    @HelpCommand
+    fun help(sender: CommandSender, help: co.aikar.commands.CommandHelp) {
+        help.showHelp()
+    }
+
+    @Default
+    @Description("Check your balance or the balance of another player")
+    @Syntax("[player]")
+    @CommandCompletion("@players")
+    fun onBalanceCommand(sender: CommandSender, @Optional playerName: String?) {
+        val target: Player? = if (playerName != null) {
+            Bukkit.getPlayer(playerName)
+        } else {
+            if (sender is Player) sender else null
+        }
+
+        if (target == null) {
+            sender.sendMessage("&#FF0000&lERROR &8| &fPlayer not found or you must specify a player.".translate())
+            return
+        }
+
+        val balance = VaultHook.getBalance(target)
+        sender.sendMessage("&#00FF00&lBALANCE &8| &f${target.name}'s balance is: $${balance}".translate())
+    }
+} 
