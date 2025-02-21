2a06f34195060b4585dba6551068daa8ffdcf684
snowypy <git@snowyjs.lol>
2025-02-21 22:41:20 +0000

    v3.25.6 (Improve LSAdminCommand with Player Name Lookup and Translation)

--- src/main/java/codes/snowy/dupeJS/lifesteal/LSAdminCommand.kt
+++ src/main/java/codes/snowy/dupeJS/lifesteal/LSAdminCommand.kt
@@ -2,6 +2,8 @@ package codes.snowy.dupeJS.lifesteal
 
 import co.aikar.commands.BaseCommand
 import co.aikar.commands.annotation.*
+import codes.snowy.dupeJS.utils.translate
+import org.bukkit.Bukkit
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
@@ -16,37 +18,54 @@ class LSAdminCommand(private val manager: LifestealManager) : BaseCommand() {
     }
 
     @Subcommand("add")
-    @Syntax("<target> <amount>")
-    fun onAdd(sender: CommandSender, target: Player, amount: Int) {
+    @Syntax("<targetName> <amount>")
+    @CommandCompletion("@players 1|2|3|4|5|6|7|8|9|10")
+    fun onAdd(sender: CommandSender, targetName: String, amount: Int) {
+        val target = Bukkit.getPlayer(targetName) ?: run {
+            sender.sendMessage("&#FF0000&nCouldn't find player with name $targetName.".translate())
+            return
+        }
         manager.addHearts(target, amount)
-        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fAdded $amount heart(s) to &#f987ca&n${target.name}&f.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fAdded $amount heart(s) to &#f987ca&n${target.name}&f.".translate())
     }
 
     @Subcommand("remove")
     @Syntax("<target> <amount>")
-    fun onRemove(sender: CommandSender, target: Player, amount: Int) {
+    @CommandCompletion("@players 1|2|3|4|5|6|7|8|9|10")
+    fun onRemove(sender: CommandSender, targetName: String, amount: Int) {
+        val target = Bukkit.getPlayer(targetName) ?: run {
+            sender.sendMessage("&#FF0000&nCouldn't find player with name $targetName.".translate())
+            return
+        }
         manager.removeHearts(target, amount)
-        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fRemoved $amount heart(s) from &#f987ca&n${target.name}&f.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fRemoved $amount heart(s) from &#f987ca&n${target.name}&f.".translate())
     }
 
     @Subcommand("giveall")
     @Syntax("<amount>")
+    @CommandCompletion("1|2|3|4|5|6|7|8|9|10")
     fun onGiveAll(sender: CommandSender, amount: Int) {
         manager.giveAllHearts(amount)
-        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to all players.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to all players.".translate())
     }
 
     @Subcommand("giveitem")
-    @Syntax("<target> <amount>")
-    fun onGiveItem(sender: CommandSender, target: Player, amount: Int) {
+    @Syntax("<targetName> <amount>")
+    @CommandCompletion("@players 1|2|3|4|5|6|7|8|9|10")
+    fun onGiveItem(sender: CommandSender, targetName: String, amount: Int) {
+        val target = Bukkit.getPlayer(targetName) ?: run {
+            sender.sendMessage("&#FF0000&nCouldn't find player with name $targetName.".translate())
+            return
+        }
         manager.giveHeartItem(target, amount)
-        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to &#f987ca&n${target.name}&f.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to &#f987ca&n${target.name}&f.".translate())
     }
 
     @Subcommand("giveallitem")
     @Syntax("<amount>")
+    @CommandCompletion("1|2|3|4|5|6|7|8|9|10")
     fun onGiveAllItem(sender: CommandSender, amount: Int) {
         manager.giveAllHeartItems(amount)
-        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to all players.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to all players.".translate())
     }
 }
