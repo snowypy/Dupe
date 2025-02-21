197521dd7731e8c7ba271a0a50991f59a8caf1b6
snowypy <git@snowyjs.lol>
2025-02-21 21:26:39 +0000

    v3.25.3 (Add Heart Item Distribution Commands to LSAdminCommand)

--- src/main/java/codes/snowy/dupeJS/lifesteal/LSAdminCommand.kt
+++ src/main/java/codes/snowy/dupeJS/lifesteal/LSAdminCommand.kt
@@ -19,20 +19,34 @@ class LSAdminCommand(private val manager: LifestealManager) : BaseCommand() {
     @Syntax("<target> <amount>")
     fun onAdd(sender: CommandSender, target: Player, amount: Int) {
         manager.addHearts(target, amount)
-        sender.sendMessage("Added $amount heart(s) to ${target.name}.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fAdded $amount heart(s) to &#f987ca&n${target.name}&f.")
     }
 
     @Subcommand("remove")
     @Syntax("<target> <amount>")
     fun onRemove(sender: CommandSender, target: Player, amount: Int) {
         manager.removeHearts(target, amount)
-        sender.sendMessage("Removed $amount heart(s) from ${target.name}.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fRemoved $amount heart(s) from &#f987ca&n${target.name}&f.")
     }
 
     @Subcommand("giveall")
     @Syntax("<amount>")
     fun onGiveAll(sender: CommandSender, amount: Int) {
         manager.giveAllHearts(amount)
-        sender.sendMessage("Gave $amount heart(s) to all players.")
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to all players.")
+    }
+
+    @Subcommand("giveitem")
+    @Syntax("<target> <amount>")
+    fun onGiveItem(sender: CommandSender, target: Player, amount: Int) {
+        manager.giveHeartItem(target, amount)
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to &#f987ca&n${target.name}&f.")
+    }
+
+    @Subcommand("giveallitem")
+    @Syntax("<amount>")
+    fun onGiveAllItem(sender: CommandSender, amount: Int) {
+        manager.giveAllHeartItems(amount)
+        sender.sendMessage("&#f987ca&lLIFESTEAL &8| &fGave $amount heart(s) to all players.")
     }
 }
