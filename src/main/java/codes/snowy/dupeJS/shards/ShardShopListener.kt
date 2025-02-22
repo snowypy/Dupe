3907c039c95334e7488e2f8800c22883973eda03
snowypy <git@snowyjs.lol>
2025-02-22 20:23:57 +0000

    v3.26.10 (Implement Cash and Shard Purchase to ShardShop Listener)

--- src/main/java/codes/snowy/dupeJS/shards/ShardShopListener.kt
+++ src/main/java/codes/snowy/dupeJS/shards/ShardShopListener.kt
@@ -7,6 +7,8 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.ClickType
+import codes.snowy.dupeJS.economy.VaultHook
 
 class ShardShopListener(private val afkManager: AFKManager) : Listener {
 
@@ -24,21 +26,52 @@ class ShardShopListener(private val afkManager: AFKManager) : Listener {
             clickedItem.itemMeta?.displayName == item.displayName
         } ?: return
 
+        when (event.click) {
+            ClickType.LEFT -> handleShardPurchase(player, shopItem.value)
+            ClickType.RIGHT -> handleCashPurchase(player, shopItem.value)
+            else -> return
+        }
+    }
+
+    private fun handleShardPurchase(player: Player, item: ShopItem) {
         val playerShards = afkManager.getShards(player.uniqueId)
-        if (playerShards < shopItem.value.price) {
-            player.sendMessage("&#9436fe&lSHARDS &8| &cYou don't have enough shards! You need ${shopItem.value.price} shards.".translate())
+        if (playerShards < item.price) {
+            player.sendMessage("&#9436fe&lSHARDS &8| &cYou don't have enough shards! You need ${item.price} shards.".translate())
+            return
+        }
+        afkManager.removeShards(player.uniqueId, item.price)
+        executeCommands(player, item)
+        player.sendMessage("&#9436fe&lSHARDS &8| &aSuccessfully purchased ${item.displayName}&a!".translate())
+    }
+
+    private fun handleCashPurchase(player: Player, item: ShopItem) {
+        val cashPrice = item.cashPrice
+        if (cashPrice == null) {
+            player.sendMessage("&#9436fe&lSHARDS &8| &cThis item cannot be purchased with cash.".translate())
             return
         }
 
-        afkManager.removeShards(player.uniqueId, shopItem.value.price)
-        
-        shopItem.value.commands.forEach { command ->
+        if (!VaultHook.hasEconomy()) {
+            player.sendMessage("&#9436fe&lSHARDS &8| &cThere was an error with the Economy.".translate())
+            return
+        }
+
+        if (VaultHook.getBalance(player) < cashPrice) {
+            player.sendMessage("&#9436fe&lSHARDS &8| &cYou don't have enough money! You need $${String.format("%.2f", cashPrice)}.".translate())
+            return
+        }
+
+        VaultHook.withdraw(player, cashPrice)
+        executeCommands(player, item)
+        player.sendMessage("&#9436fe&lSHARDS &8| &aSuccessfully purchased ${item.displayName}&a for $${String.format("%.2f", cashPrice)}!".translate())
+    }
+
+    private fun executeCommands(player: Player, item: ShopItem) {
+        item.commands.forEach { command ->
             Bukkit.dispatchCommand(
                 Bukkit.getConsoleSender(),
                 command.replace("%player%", player.name)
             )
         }
-
-        player.sendMessage("&#9436fe&lSHARDS &8| &aSuccessfully purchased ${shopItem.value.displayName}&a!".translate())
     }
 } 
